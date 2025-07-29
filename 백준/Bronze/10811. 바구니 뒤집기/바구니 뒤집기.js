9cdda6720ffbcec99d682264193303c6cdc07f72
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2025-07-30 01:30:51 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 108 ms, Memory: 9388 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [N, M] = input[0].split(" ").map(Number);
+
+const baskets = Array(N)
+  .fill(0)
+  .map((_, index) => index + 1);
+
+for (let i = 1; i <= M; i++) {
+  const [num1, num2] = input[i].split(" ").map(Number);
+
+  if (num1 === num2) continue;
+
+  const temp = baskets.slice(num1 - 1, num2);
+
+  temp.reverse();
+
+  let k = 0;
+
+  for (let j = num1 - 1; j < num2; j++) {
+    baskets[j] = temp[k++];
+  }
+}
+
+console.log(baskets.join(" "));
