3cbe56c8dab4f971ff936f8262b183ac72e29e84
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2025-08-02 01:41:08 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 96 ms, Memory: 9436 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.js
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.js
@@ -1,28 +1,40 @@
-const fs = require("fs");
+// 백준 Node.js 환경에서 입력을 받는 표준 코드
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-let input = fs.readFileSync(0).toString().trim().split('\n');
+// 첫 번째 줄은 단어의 개수 N
+const N = Number(input[0]);
+// 그룹 단어의 개수를 셀 변수
+let groupWordCount = 0;
 
-const N = input[0];
+// 두 번째 줄부터 각 단어를 순회
+for (let i = 1; i <= N; i++) {
+  const word = input[i];
+  const seen = new Set(); // 이미 나타난 문자를 기록할 Set
+  let lastChar = ''; // 바로 이전 문자를 저장할 변수
+  let isGroupWord = true; // 현재 단어가 그룹 단어인지 판별하는 플래그
 
-const words = [];
-
-let count = 0;
-
-for (let i = 1; i <= N; i++) words.push(input[i].replace("\r", ""));
-
-for (const word of words) {
-  const alphabets = Array(26).fill(0); // 소문자는 97, 인덱스는 0
-
-  let temp = word[0];
-  alphabets[temp.charCodeAt(0) - 97]++;
-
-  for (let i = 0; i < word.length; i++) {
-    if (temp === word[i]) continue;
-    alphabets[word[i].charCodeAt(0) - 97]++;
-    temp = word[i];
+  for (const char of word) {
+    // 이전 문자와 현재 문자가 다를 때
+    if (char !== lastChar) {
+      // 이전에 이미 나타났던 문자라면 그룹 단어가 아님
+      if (seen.has(char)) {
+        isGroupWord = false;
+        break; // 더 이상 확인할 필요가 없으므로 현재 단어의 루프를 종료
+      }
+      // 처음 보는 문자라면 seen에 추가
+      seen.add(char);
+      // 이전 문자 업데이트
+      lastChar = char;
+    }
+    // 이전 문자와 현재 문자가 같으면 아무것도 하지 않고 계속 진행
   }
 
-  if (alphabets.every((value) => value < 2)) count++;
+  // 루프를 무사히 통과했다면 그룹 단어이므로 카운트 증가
+  if (isGroupWord) {
+    groupWordCount++;
+  }
 }
 
-console.log(count);
+// 최종 결과 출력
+console.log(groupWordCount);
