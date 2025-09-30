d2e51c727a44030f1fdc9739d9ea4132c3a3bd3c
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2025-09-30 09:43:24 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 92 ms, Memory: 9392 KB -BaekjoonHub

--- 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.js
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.js
@@ -3,46 +3,21 @@ const fs = require("fs");
 const input = fs.readFileSync("./dev/stdin").toString().trim().split("\n");
 
 const N = Number(input[0]);
-const currentLine = input[1].split(" ").map(Number);
+const arr = input[1].split(" ").map(Number);
 
-let currentOrder = 1;
-
-const waitingStack = [];
+let number = 1;
+const stack = [];
 
 for (let i = 0; i < N; i++) {
-  const student = currentLine[i];
+  stack.push(arr[i]);
 
-  // 현재 학생이 받을 차례가 아니라면
-  if (student !== currentOrder) {
-    while (
-      waitingStack.length > 0 &&
-      waitingStack[waitingStack.length - 1] === currentOrder
-    ) {
-      waitingStack.pop();
-      currentOrder++;
+  while (true) {
+    if (stack[stack.length - 1] !== number) {
+      break;
     }
-
-    if (student !== currentOrder) {
-      waitingStack.push(student);
-    } else {
-      currentOrder++;
-    }
-  } else {
-    currentOrder++;
+    stack.pop();
+    number++;
   }
 }
 
-while (waitingStack.length > 0) {
-  if (waitingStack[waitingStack.length - 1] === currentOrder) {
-    waitingStack.pop();
-    currentOrder++;
-  } else {
-    break;
-  }
-}
-
-if (currentOrder === N + 1) {
-  console.log("Nice");
-} else {
-  console.log("Sad");
-}
+console.log(stack.length === 0 ? "Nice" : "Sad");
