8ecafa5f73cc60c959a23876cea4da125f5a2d65
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2025-11-03 00:42:34 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 39.04 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,9 @@
+function solution(arr) {
+  const answer = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (answer[answer.length - 1] === arr[i]) continue;
+    answer.push(arr[i]);
+  }
+  return answer;
+}
