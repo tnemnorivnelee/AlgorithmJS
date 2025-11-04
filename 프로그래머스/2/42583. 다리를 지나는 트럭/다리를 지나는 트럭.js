f281c391bb73166f48dd405d043f3e096347fce9
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2025-11-04 13:15:14 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 3987.78 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,29 +1,27 @@
 function solution(bridge_length, weight, truck_weights) {
     var answer = 0;
-    let arrivedTrucks = truck_weights.length;
     
-    const truckInBridge = Array.from({length: bridge_length}).fill(0);
+    const destination = [];
+    const trucksInBridge = Array.from({length: bridge_length}).fill(0);
+    let truckCount = truck_weights.length;
     
-    while (arrivedTrucks > 0) {
-        const exitingTruck = truckInBridge.shift();
+    while (destination.length !== truckCount) {
+        const getFirstTruckInBridge = trucksInBridge.shift();
         
-        if (exitingTruck > 0) { 
-            arrivedTrucks--;      
-        }       
-        
-        const totalWeightInBridge = truckInBridge.reduce((acc, cur) => acc + cur, 0);
-        
-        if (truck_weights.length > 0 && totalWeightInBridge + truck_weights[0] <= weight) {
-            const getFirstWaiting = truck_weights.shift();
-            truckInBridge.push(getFirstWaiting);
-        } else {
-            truckInBridge.push(0);
+        if (getFirstTruckInBridge) {
+            destination.push(getFirstTruckInBridge);
         }
         
-        answer++;
+        const totalWeightInBridge = trucksInBridge.reduce((acc, cur) => acc + cur, 0);
         
+        if (totalWeightInBridge +  truck_weights[0] <= weight) {
+            trucksInBridge.push(truck_weights.shift());
+        } else {
+            trucksInBridge.push(0);
+        }
+        
+     answer++;
     }
-    
     
     return answer;
 }
