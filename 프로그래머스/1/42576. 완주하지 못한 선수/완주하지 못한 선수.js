fab981961fb4798b14f07ffa2010fc3c592462a5
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2025-12-23 15:56:08 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 61.32 ms, Memory: 66.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -2,18 +2,13 @@ function solution(participant, completion) {
     const mapParticipant = new Map();
     
     for (let i = 0; i < participant.length; i++) {
-        if (mapParticipant.has(participant[i])) {
-            mapParticipant.set(participant[i], mapParticipant.get(participant[i]) + 1);    
-        } else {
-            mapParticipant.set(participant[i], 1);
-        }
+        const a = participant[i];
+        const b = completion[i];
+        
+        mapParticipant.set(a, (mapParticipant.get(a) || 0) + 1);
+        mapParticipant.set(b, (mapParticipant.get(b) || 0) - 1);
     }
-    
-    for (let i = 0; i < completion.length; i++) {
-        if (mapParticipant.has(completion[i])) {
-            mapParticipant.set(completion[i], mapParticipant.get(completion[i]) - 1);
-        }
-    }
+
     
     for (const [key, value] of mapParticipant) {
         if (value) return key;
