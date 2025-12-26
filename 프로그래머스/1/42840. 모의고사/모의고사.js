0c1099d31cf5a1fb4e67a9e37d05427129ff42ae
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2025-12-26 16:54:35 +0900

    [level 1] Title: 모의고사, Time: 5.62 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,14 +1,13 @@
 function solution(answers) {
-    const sufoja1 = [1, 2, 3, 4, 5];
-    const sufoja2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    const sufoja3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    const sufojas = [
+        [1, 2, 3, 4, 5],
+        [2, 1, 2, 3, 2, 4, 2, 5],
+        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    ];
     
-    const sufojas = [sufoja1, sufoja2, sufoja3];
-    
-    // console.log(sufojas);
-    
-    const scores = [0, 0, 0];
-    
+    const scores = new Array(sufojas.length).fill(0);
+
+
     for (let i = 0; i < sufojas.length; i++) {
         for (let j = 0; j < answers.length; j++) {
             if (answers[j] === sufojas[i][j % sufojas[i].length]) {
@@ -17,16 +16,16 @@ function solution(answers) {
         }
     }
     
-    // console.log(scores);
-    
     const maxScore = Math.max(...scores);
     
     const answer = [];
     
     for (let i = 0; i < scores.length; i++) {
-        if (scores[i] === maxScore) answer.push(i + 1);
+        if (scores[i] === maxScore) {
+            answer.push(i + 1);
+        }
     }
     
-    // console.log(answer);
     return answer;
+
 }
