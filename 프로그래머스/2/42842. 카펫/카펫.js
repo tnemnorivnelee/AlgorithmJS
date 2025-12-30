68ceae38cd6f8af1abca4df76467c9125927295d
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2025-12-30 14:47:05 +0900

    [level 2] Title: 카펫, Time: 0.28 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,13 +1,23 @@
 function solution(brown, yellow) {
+    // 브라운 + 옐로우 = 가로 * 세로
+    // 가로, 세로 = 약수 중 하나
+    // 가로 >= 세로
+    // 가로 = 옐로우 가로길이 + 2
+    // 세로 = 옐로우 세로길이 + 2
     
     const divisors = [];
     
+    
     for (let i = 1; i <= Math.sqrt(yellow); i++) {
-        if (yellow % i === 0 && (i + 2) * (yellow / i + 2) === brown + yellow) {
-            divisors.push(yellow / i);
-            divisors.push(i)
+        const w = yellow / i;
+        const h = i;
+        if (yellow % i === 0 && (w + 2) * (h + 2) === brown + yellow) {
+            divisors.push(w + 2);
+            divisors.push(h + 2);
         }
     }
     
-    return [divisors[0] + 2, divisors[1] + 2];
+    // console.log(divisors);
+    
+    return divisors;
 }
