dd5ce14b73be7c2c89ed02024240b48d13a1e85a
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2026-01-03 19:17:32 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,11 +1,24 @@
 function solution(s) {
+    // 첫 문자는 대문자, 나머지 문자는 전부 소문자 처리
+    // 공백 문자가 연속으로 나올 수 있음
+    
     const words = s.split(" ");
     
-    for (let i = 0; i < words.length; i++) {
-        if (words[i] === "" ) continue;
-        
-        words[i] = words[i][0].toUpperCase() + words[i].substring(1).toLowerCase();
-    }
+    // console.log(words);
     
-    return words.join(" ");
+    const jcString = words.map((value, index) => {
+        if (value === "") return;
+        
+        const upper = value[0].toUpperCase();
+        const lower = value.substring(1).toLowerCase();
+        
+        return upper + lower;
+    });
+    
+    // console.log(jcString);
+    
+    return jcString.join(" ");
+    
+    
+    
 }
