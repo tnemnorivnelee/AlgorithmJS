b0ece97ab787d5a052782aa2713af0ef1c5a252d
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2026-01-03 19:53:11 +0900

    [level 2] Title: 기능개발, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,16 +1,28 @@
 function solution(progresses, speeds) {
-    let answer = [0];
-    let days = progresses.map((progress, index) => Math.ceil((100 - progress) / speeds[index]));
-    let maxDay = days[0];
+    // 각 기능은 진도 100% 일 때 서비스에 반영 가능.
+    // 뒤에 있는 기능이 앞에 있는 기능보다 먼저 개발될 수 있음.
+    // 이때 뒤에 있는 기능은 앞에 있는 기능이 배포될 때 함께 배포됨.
     
-    for (let i = 0, j = 0; i < days.length; i++) {
-        if (days[i] <= maxDay) {
-            answer[j] += 1;
+    const queue = Array.from({length: progresses.length }).fill(0);
+    
+    for (let i = 0; i < queue.length; i++) {
+        queue[i] = Math.ceil((100 - progresses[i]) / speeds[i]);
+    }
+    
+    let answer = [];
+    let maxDay = queue[0];
+    let count = 1;
+    
+    for (let i = 1; i < queue.length; i++) {
+        if (queue[i] <= maxDay) {
+            count++;
         } else {
-            maxDay = days[i];
-            answer[++j] = 1;
+            answer.push(count);
+            maxDay = queue[i];
+            count = 1;
         }
     }
+    answer.push(count);
     
     return answer;
 }
