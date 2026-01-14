e34cc19fdd40d2a415ce3acb9f665c6ae0381fd7
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2026-01-14 20:37:28 +0900

    [level 2] Title: 의상, Time: 0.28 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,24 +1,22 @@
 function solution(clothes) {
-    
     const clothesMap = new Map();
     
     for (let i = 0; i < clothes.length; i++) {
-        const [value, key] = clothes[i];
+        const [wear, category] = clothes[i];
         
-        if (clothesMap.has(key)) {
-            clothesMap.get(key).push(value);
+        if (clothesMap.has(category)) {
+            clothesMap.get(category).push(wear);
         } else {
-            clothesMap.set(key, [value]);
+            clothesMap.set(category, [wear]);
         }
     }
     
-    console.log(clothesMap);
-
-    let answer = 1;
+   let answer = 1;
     
-    for (let [key, value] of clothesMap) {
+    for (const [key, value] of clothesMap) {
         answer *= value.length + 1;
     }
     
     return answer - 1;
+    
 }
