91987f212995ac7cc3cb5ceea6a60b0c384f63dd
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2026-01-15 20:23:36 +0900

    [level 2] Title: 타겟 넘버, Time: 40.10 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,24 +1,27 @@
 function solution(numbers, target) {
-    let answer = 0;
     
-    // dfs 3단계 공식
+     // dfs 3단계 공식
     // 1. 탈출 조건 : "언제 끝낼 것인가?" (가장 중요, 없으면 무한루프)
     // 2. 수행 동작 : "정답을 찾거나, 방문 체크를 하는 곳"
     // 3. 다음 단계 호출 : "자식 노드나 다음 선택지로 이동"
     
-    function dfs(index, currentSum) {
-        if (index === numbers.length) {
-            if (target === currentSum) {
+    let answer = 0;
+    
+    function dfs(index, sum) {
+        
+        if (numbers.length === index) {
+            if (sum === target) {
                 answer++;
             }
             return;
         }
         
-        dfs(index + 1, currentSum + numbers[index]);
-        dfs(index + 1, currentSum - numbers[index]);
+        dfs(index + 1, sum + numbers[index]);
+        dfs(index + 1, sum - numbers[index]);
     }
     
     dfs(0, 0);
     
     return answer;
+    
 }
