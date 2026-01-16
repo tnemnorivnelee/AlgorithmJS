90382ca26488936717ce721aeea0b21ad681bf20
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2026-01-16 16:20:10 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 30.18 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 37.8 MB, 시간: 37.90 ms
+메모리: 37.4 MB, 시간: 30.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 10일 22:31:04
+2026년 01월 16일 16:20:07
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,38 +1,26 @@
 function solution(maps) {
+    const n = maps.length;
+    const m = maps[0].length;
+    
+    const dy = [-1, 1, 0, 0];
+    const dx = [0, 0, -1, 1];
+    
     const queue = [];
+    queue.push([0, 0]);
     
-    const direction = [
-        [-1, 0], // 상 (row - 1)
-        [1, 0],  // 하 (row + 1)
-        [0, -1], // 좌 (col - 1)
-        [0, 1]   // 우 (col + 1)
-    ];
-    
-    function bfs(row, col, distance) {
-        maps[row][col] = 0;
-        queue.push([row, col, distance]);
+    while (queue.length > 0) {
+        const [y, x] = queue.shift();
         
-        while (queue.length > 0) {
-            const [currentRow, currentCol, currentDist] = queue.shift();
+        for (let i = 0; i < 4; i++) {
+            const ny = y + dy[i];
+            const nx = x + dx[i];
             
-            const n = maps.length;
-            const m = maps[0].length;
-            
-            if (currentRow === n - 1 && currentCol === m - 1) {
-                return currentDist;
-            }            
-            
-            for (let i = 0; i < direction.length; i++) {
-                const nextRow = currentRow + direction[i][0];
-                const nextCol = currentCol + direction[i][1];
-
-                if (0 <= nextRow && nextRow < n && 0 <= nextCol && nextCol < m && maps[nextRow][nextCol] === 1) {    
-                    maps[nextRow][nextCol] = 0;
-                    queue.push([nextRow, nextCol, currentDist + 1]);
-                }
+            if (0 <= ny && ny < n && 0 <= nx && nx < m && maps[ny][nx] === 1) {
+                maps[ny][nx] = maps[y][x] + 1;
+                
+                queue.push([ny, nx]);
             }
         }
-        return -1;
     }
-    return bfs(0, 0, 1);
+    return maps[n - 1][m - 1] > 1 ? maps[n - 1][m - 1] : -1;
 }
