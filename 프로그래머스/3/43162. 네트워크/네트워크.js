763a9cec5cd6b2139d305413c0cded7cecbc23ab
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2026-01-22 14:19:19 +0900

    [level 3] Title: 네트워크, Time: 0.76 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,37 +1,32 @@
 function solution(n, computers) {
-    // 1. 미방문 처리된 배열을 생성.
     const visited = new Array(n).fill(false);
-    
-    // 2. 네트워크 개수를 샐 변수 생성.
     let answer = 0;
     
-    // 3. dfs 함수 생성
-    function dfs(node) {
-        // 3-1. 종료조건
-        if (visited[node]) {
+    function dfs(current) {
+        // 1. 종료 조건
+        if (visited[current]) {
             return;
         }
         
-        // 3-2. 실행조건
-        visited[node] = true;
+        // 2. 실행 조건
+        visited[current] = true;
         
-        // 3-3. 다음 실행
+        // 3. 다음 실행
         for (let i = 0; i < n; i++) {
-            // 연결되어 있고, 아직 방문하지 않았다면?
-            // i 로 이동해서 dfs 다시 실행.
-            if (computers[node][i] === 1 && !visited[i]) {
+            if (computers[i][current] === 1 && !visited[i]) {
                 dfs(i);
             }
         }
+        
+        
     }
     
-    // 4. 모든 컴퓨터를 하나씩 돌아보기.
     for (let i = 0; i < n; i++) {
         if (!visited[i]) {
             answer++;
             dfs(i);
         }
     }
-
+    
     return answer;
 }
