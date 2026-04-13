ec57a5616328b7b05dba4a628d4b8e8867cc8d66
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2026-04-13 23:32:00 +0900

    [Silver IV] Title: 동전 0, Time: 96 ms, Memory: 9592 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.js
+++ 백준/Silver/11047. 동전 0/동전 0.js
@@ -3,14 +3,15 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split("\n");
 
 function solution(input) {
-  let [N, K] = input.shift().split(" ").map(Number);
-  const units = input.map(Number).sort((a, b) => b - a);
+  let [N, K] = input[0].split(" ").map(Number);
   let result = 0;
 
-  for (let i = 0; i < N; i++) {
-    if (units[i] <= K) {
-      result += Math.floor(K / units[i]);
-      K %= units[i];
+  for (let i = N; i >= 0; i--) {
+    let coin = Number(input[i]);
+
+    if (coin <= K) {
+      result += Math.floor(K / coin);
+      K %= coin;
     }
   }
 
