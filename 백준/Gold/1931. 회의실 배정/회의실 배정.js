b4c0885af54bb100731aecb0646f1a4c43cc30c4
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2026-04-14 16:42:55 +0900

    [Gold V] Title: 회의실 배정, Time: 384 ms, Memory: 45412 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.js
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.js
@@ -4,25 +4,30 @@ const input = fs.readFileSync(0).toString().trim().split("\n");
 
 function solution(input) {
   const N = Number(input[0]);
-  const times = input.slice(1).sort((a, b) => {
-    const [aFirst, aSecond] = a.split(" ").map(Number);
-    const [bFirst, bSecond] = b.split(" ").map(Number);
+  const times = [];
 
-    return aSecond === bSecond ? aFirst - bFirst : aSecond - bSecond;
+  let count = 0;
+  let endTime = 0;
+
+  for (let i = 1; i <= N; i++) {
+    const [start, end] = input[i].split(" ").map(Number);
+    times.push([start, end]);
+  }
+
+  times.sort((a, b) => {
+    return a[1] === b[1] ? a[0] - b[0] : a[1] - b[1];
   });
 
 
-  const result = [];
+  for (let i = 0; i < N; i++) {
+    const [nextStartTime, nextEndTime] = times[i];
 
-  result.push(times[0]);
-
-  for (let i = 1; i < times.length; i++) {
-    const prevTime = result[result.length - 1].split(" ").map(Number)[1];
-    const startTime = times[i].split(" ").map(Number)[0];
-
-    if (startTime >= prevTime) result.push(times[i]);
+    if (endTime <= nextStartTime) {
+      count++;
+      endTime = nextEndTime;
+    }
   }
-  console.log(result.length);
+  console.log(count);
 }
 
 solution(input);
