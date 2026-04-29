16860c8a5d0bc92ed9adee0d604e7ba339103d04
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2026-04-29 16:17:17 +0900

    [level 1] Title: 체육복, Time: 0.32 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,40 +1,31 @@
 function solution(n, lost, reserve) {
-    const students = new Array(n + 2).fill(1);
-
-    // 잃어버린 학생
-    for (let i = 0; i < lost.length; i++) {
-        const studentNum = lost[i];
-        students[studentNum]--;
-    }
-    
-    // 여벌이 있는 학생
-    for (let i = 0; i < reserve.length; i++) {
-        const studentNum = reserve[i];
-        students[studentNum]++;
-    }
-    
-    // console.log(students);
-    
     let answer = 0;
     
-    for (let i = 1; i <= n; i++) {
-        
+    const students = Array.from({length: n}).fill(1);
+    
+    for (let i = 0; i < n; i++) {
+        if (lost.includes(i + 1)) students[i]--;
+        if (reserve.includes(i + 1)) students[i]++;
+    }
+    
+ 
+    for (let i = 0; i < n; i++) {
         if (students[i] === 0) {
             if (students[i - 1] === 2) {
                 students[i]++;
                 students[i - 1]--;
+                continue;
             } else if (students[i + 1] === 2) {
-                students[i]++
+                students[i]++;
                 students[i + 1]--;
+                continue;
             }
         }
     }
     
-    // console.log(students);
-    
-    for (let i = 1; i <= n; i++) {
-        if (students[i] >= 1) answer++;
-    }
+    students.forEach((value) => {
+        if (value) answer++;
+    })
     
     return answer;
 }
