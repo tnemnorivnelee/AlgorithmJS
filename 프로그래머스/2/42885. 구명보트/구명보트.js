0e3ea74bbe528057d771d017e9cd9b271493e2a4
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2026-05-04 17:58:12 +0900

    [level 2] Title: 구명보트, Time: 37.28 ms, Memory: 38.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -0,0 +1,29 @@
+function solution(people, limit) {
+    var answer = 0;
+    
+    people.sort((a,b) => b - a);
+    
+    console.log(people);
+    
+    let leftIdx = 0;
+    let rightIdx = people.length - 1;
+    
+    while (leftIdx <= rightIdx) {
+        if (leftIdx === rightIdx) {
+            answer++;
+            break;
+        }
+        
+        if (people[leftIdx] + people[rightIdx] > limit) {
+            leftIdx++;
+            answer++;
+        } else if (people[leftIdx] + people[rightIdx] <= limit) {
+            leftIdx++;
+            rightIdx--;
+            answer++;
+        }
+    }
+    
+    
+    return answer;
+}
