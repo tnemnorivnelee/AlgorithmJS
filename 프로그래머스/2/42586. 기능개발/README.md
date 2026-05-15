481b5c9f04bad649c0c7006e6bd29800fc0d9ad6
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2026-05-15 15:27:38 +0900

    [level 2] Title: 기능개발, Time: 3.44 ms, Memory: 32.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.29 ms
+메모리: 32.5 MB, 시간: 3.44 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 05일 15:54:48
+2026년 05월 15일 15:27:33
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,31 +1,28 @@
 function solution(progresses, speeds) {
+    var answer = [];
     
-    const days = Array.from({length: progresses.length}).fill(0);
+    const days = Array.from({ length: progresses.length }).fill(0);
     
     for (let i = 0; i < progresses.length; i++) {
         days[i] = Math.ceil((100 - progresses[i]) / speeds[i]);
     }
     
-    // console.log(days);
+    console.log(days);
     
     let maxDays = days[0];
     let count = 1;
     
-    const answer = [];
-    
     for (let i = 1; i < days.length; i++) {
-        if (maxDays < days[i]) {
+        if (days[i] <= maxDays) {
+            count++;
+        } else {
             answer.push(count);
             count = 1;
             maxDays = days[i];
-        } else {
-            count++;
         }
     }
     
     answer.push(count);
-    
-    // console.log(answer);
     
     return answer;
 }
