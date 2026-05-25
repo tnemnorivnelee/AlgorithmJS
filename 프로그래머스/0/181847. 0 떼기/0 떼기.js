b064c9a31c103c491b9e5f2a02d28ddf200e547d
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2026-05-26 02:15:47 +0900

    [level 0] Title: 0 떼기, Time: 0.04 ms, Memory: 32.6 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.js
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.js
@@ -1,7 +1,3 @@
 function solution(n_str) {
-    var answer = '';
-
-    for (let i = 0; i < n_str.length; i++) {
-        if (n_str[i] !== "0") return n_str.slice(i);
-    }
+    return Number(n_str).toString();
 }
