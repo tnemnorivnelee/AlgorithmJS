4f526b0c932425197df88169e27adf02eabd98a1
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2026-05-26 20:22:10 +0900

    [level 1] Title: 삼총사, Time: 0.16 ms, Memory: 32.9 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.js
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -1,13 +1,19 @@
 function solution(number) {
     var answer = 0;
     
-    for (let i = 0; i < number.length; i++) {
-        for (let j = i + 1; j < number.length; j++) {
-            for (let k = j + 1; k < number.length; k++) {
-                if (number[i] + number[j] + number[k] === 0) answer++;
-            }
+    function dfs(index, count, sum) {
+        if (count === 3) {
+            if (sum === 0) answer++;
+            return;
+        }
+        
+        for (let i = index; i < number.length; i++) {
+            dfs(i + 1, count + 1, sum + number[i]);
         }
     }
+    
+    
+    dfs(0, 0, 0);
     
     return answer;
 }
