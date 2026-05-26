f98ddd75bc9ecea912c908c0265e40da5d5783d2
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2026-05-26 15:22:01 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.08 ms, Memory: 32.6 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,4 +1,5 @@
 function solution(my_string, letter) {
     // return [...my_string].filter((v) => v !== letter).join("");
-    return my_string.split(letter).join("");
+    // return my_string.split(letter).join("");
+    return my_string.replaceAll(letter, "");    
 }
