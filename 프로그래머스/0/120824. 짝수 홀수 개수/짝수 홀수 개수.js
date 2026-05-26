c9f493b1de51567dd3152784cb2ef4c338ec9569
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2026-05-26 15:16:06 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.11 ms, Memory: 32.9 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,7 +1,6 @@
 function solution(num_list) {
-    return num_list.reduce((acc, cur) => {
-        if (cur % 2 === 0) acc[0] += 1;
-        else acc[1] += 1;
-        return acc;
-    }, [0, 0]);
+    return [
+        num_list.filter((v) => v % 2 === 0).length,
+        num_list.filter((v) => v % 2 !== 0).length
+    ]
 }
