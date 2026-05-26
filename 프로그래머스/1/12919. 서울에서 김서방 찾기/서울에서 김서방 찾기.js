8beee57f359c8dc87d7b9118c4c2b6ad725514b7
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2026-05-26 19:04:42 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.05 ms, Memory: 32.6 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,3 +1,4 @@
 function solution(seoul) {
-    return `김서방은 ${seoul.indexOf("Kim")}에 있다`
+    // return `김서방은 ${seoul.indexOf("Kim")}에 있다`
+    return `김서방은 ${seoul.findIndex(v => v === "Kim")}에 있다`
 }
