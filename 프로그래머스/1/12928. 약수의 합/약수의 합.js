2a5655d724c979600ebe9b04c9aa821a81025efe
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2026-05-26 15:41:38 +0900

    [level 1] Title: 약수의 합, Time: 0.17 ms, Memory: 32.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    let answer = [];
+    
+    for (let i = 1; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) {
+            answer.push(i);
+            answer.push(n / i);
+        }
+    }
+    return [...new Set(answer)].reduce((acc, cur) => acc + cur, 0);
+}
