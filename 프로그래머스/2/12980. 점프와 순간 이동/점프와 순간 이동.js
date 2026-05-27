b0f291e70464e10c8192683a432d3f7e7540ae1d
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2026-05-27 14:30:09 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.05 ms, Memory: 32.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -0,0 +1,15 @@
+function solution(n)
+{
+    var ans = 0;
+    
+    while (n > 0) {
+        if (n % 2 !== 0) {
+            n--;
+            ans++;
+        } else {
+            n /= 2;
+        }
+    }
+
+    return ans;
+}
