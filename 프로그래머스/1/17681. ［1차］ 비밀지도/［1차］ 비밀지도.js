916a5b4208b23a26475aacb2f323f638bcbed608
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2026-05-28 10:07:00 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.14 ms, Memory: 32.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -8,33 +8,41 @@
 
 // 암호화된 배열은 지도의 각 가로줄에서 벽을 1, 공백을 0 으로 부호화했을 때 얻어지는 이진수에 해당하는 값의 배열
 
-
 function solution(n, arr1, arr2) {
-    var answer = [];
-    
-    const map1 = arr1.reduce((acc, cur) => [...acc, cur.toString(2).padStart(n, "0")], []);
-    const map2 = arr2.reduce((acc, cur) => [...acc, cur.toString(2).padStart(n, "0")], []);
-    const totalMap = [];
-    
-    for (let i = 0; i < n; i++) {
-        let row = "";
+    return arr1.map((v, i) => {
+        const combined = (v | arr2[i]).toString(2).padStart(n, "0");
         
-        for (let j = 0; j < n; j++) {
-            if (map1[i][j] === "0" && map2[i][j] === "0") {
-                row += "1";
-            } else {
-                row += "0";
-            }
-        }
-        totalMap.push(row);
-    }
-    
-    for (let i = 0; i < n; i++) {
-        totalMap[i] = totalMap[i].replaceAll("0", "#");
-        totalMap[i] = totalMap[i].replaceAll("1", " ");
-    }
-    
-    console.log(totalMap);
-    
-    return totalMap;
+        return combined.replace(/1/g, "#").replace(/0/g, " ");
+    });
 }
+
+
+// function solution(n, arr1, arr2) {
+//     var answer = [];
+    
+//     const map1 = arr1.reduce((acc, cur) => [...acc, cur.toString(2).padStart(n, "0")], []);
+//     const map2 = arr2.reduce((acc, cur) => [...acc, cur.toString(2).padStart(n, "0")], []);
+//     const totalMap = [];
+    
+//     for (let i = 0; i < n; i++) {
+//         let row = "";
+        
+//         for (let j = 0; j < n; j++) {
+//             if (map1[i][j] === "0" && map2[i][j] === "0") {
+//                 row += "1";
+//             } else {
+//                 row += "0";
+//             }
+//         }
+//         totalMap.push(row);
+//     }
+    
+//     for (let i = 0; i < n; i++) {
+//         totalMap[i] = totalMap[i].replaceAll("0", "#");
+//         totalMap[i] = totalMap[i].replaceAll("1", " ");
+//     }
+    
+//     console.log(totalMap);
+    
+//     return totalMap;
+// }
