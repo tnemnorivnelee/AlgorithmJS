5bc9c735306d4fb1ad6c1043d62ec14f01ff7273
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2026-05-28 10:49:59 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.09 ms, Memory: 32.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -0,0 +1,33 @@
+// 로또는 1부터 45까지의 숫자 중에 6개를 찍어서 맞히는 복권
+// 민우는 발표일을 기다리는데, 민우동생이 로또에 낙서를 하여, 일부 번호를 알아볼 수 없음
+
+// 민우는 자신이 구매한 로또의 예상 당첨최고순위, 예상 당첨최저순위를 알아보고 싶음
+
+// 알아볼 수 없는 번호를 0 으로 표기
+
+
+function solution(lottos, win_nums) {
+    
+    if (lottos.every((v) => v === 0)) return [1, 6];
+    
+    let lowCount = 0;
+    
+    for (let i = 0; i < lottos.length; i++) {
+        if (win_nums.includes(lottos[i])) lowCount++;    
+    }
+    
+    const zeroCount = lottos.filter((v) => v === 0).length;
+    const highCount = lowCount + zeroCount;
+    
+    switch(highCount) {
+        case 6: return [1, 1 + zeroCount];
+        case 5: return [2, 2 + zeroCount];
+        case 4: return [3, 3 + zeroCount];
+        case 3: return [4, 4 + zeroCount];
+        case 2: return [5, 5 + zeroCount];
+        default: return [6, 6];
+    }
+    
+    console.log(lowCount);
+    console.log(zeroCount);
+}
