aec37351bbe43047cbc3ec37f545e9e2f146b76c
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2026-06-01 15:09:25 +0900

    [level 2] Title: n^2 배열 자르기, Time: 4.75 ms, Memory: 48.6 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
@@ -1,20 +1,21 @@
+// n = 3
+// (0, 2) -> 3 | 2 / 3 = 0, 2 % 3 = 2
+// (1, 0) -> 2 | 3 / 3 = 1, 3 % 3 = 0
+// (1, 1) -> 2 | 4 / 3 = 1, 4 % 3 = 1
+// (1, 2) -> 3 | 5 / 3 = 1, 5 % 3 = 2
+// (몫, 나머지)
+// 몫 = i / n
+// 나머지 = i % n
+
 function solution(n, left, right) {
-    // 결과를 담을 빈 배열을 생성합니다.
-    const result = [];
+    var answer = [];
     
-    // left부터 right까지만 반복문을 실행합니다. (전체 범위를 돌지 않습니다!)
     for (let i = left; i <= right; i++) {
-        
-        // 현재 인덱스(i)가 2차원 배열에서 몇 번째 행(row)인지 계산합니다.
         const row = Math.floor(i / n);
-        
-        // 현재 인덱스(i)가 2차원 배열에서 몇 번째 열(col)인지 계산합니다.
         const col = i % n;
         
-        // 행과 열 중 더 큰 값에 1을 더하여 결과 배열의 끝에 추가합니다.
-        result.push(Math.max(row, col) + 1);
+        answer.push(Math.max(row, col) + 1);
     }
     
-    // 최종 완성된 배열을 반환합니다.
-    return result;
+    return answer;
 }
