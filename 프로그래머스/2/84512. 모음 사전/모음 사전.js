0b107ef4757f12e321e86eedd252eb5ffdd03e98
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2026-06-04 14:10:34 +0900

    [level 2] Title: 모음 사전, Time: 0.47 ms, Memory: 32.8 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,8 +1,3 @@
-// 사전에 알파벳 보음 A E I O U 만을 사용하여 만들 수 있는
-// 길이 5 이하의 모든 단어가 수록되어 있다
-// 사전에 첫 번째 단어는 A 이고, 그 다음은 AA 이며, 마지막 단어는 UUUUU 이다.
-// 단어 하나 word 가 매개변수로 주어질 때, 이 단어가 사전에서 몇 번째 단어인지 return
-
 function solution(word) {
     let answer = 0;
     let count = 0;
@@ -10,20 +5,21 @@ function solution(word) {
     const alphabets = ['A', 'E', 'I', 'O', 'U'];
     
     function dfs(currentWord, targetWord) {
-        if (answer !== 0) return;
-        
-        if (word === currentWord) {
+        // 1. 종료 조건
+        if (currentWord === targetWord) {
             answer = count;
             return;
         }
         
         if (currentWord.length === 5) return;
         
+        // 2. 실행 로직
         for (let i = 0; i < alphabets.length; i++) {
             count++;
             dfs(currentWord + alphabets[i], targetWord);
         }
     }
+    
     
     dfs("", word);
     
