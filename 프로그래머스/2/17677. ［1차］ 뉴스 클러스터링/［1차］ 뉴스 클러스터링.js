ea6926ec2bbf0180294ce15470b65ea51fdea28f
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2026-06-05 14:54:32 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.69 ms, Memory: 32.9 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -1,74 +1,36 @@
-// 여러 언론사에서 쏟아지는 뉴스
-// 특히 속보성 뉴스를 보면 비슷한 제목의 기사가 많아 정작 필요한 기사를 찾기가 어렵다
-// DAUM 뉴스의 개발 업무를 맡게 된 신입사원 튜브는 사용자들이 편리하게 다양한 뉴스를 찾아볼 수 있도록 문제점을 개선하는 업무를 맡게 되었다
-// 개발의 방향을 잡기 위해 튜브는 우선 최근 화제가 되고 있는 카카오 신입 개발자 공채 관련 기사를 검색
-
-// 카카오 첫 공개..'블라인드'방식 채용
-// 카카오, 합경 후 첫 공채.. 블라인드 전형으로 개발자 채용
-// 이하 4개의 기사...
-
-// 기사의 제목을 기준으로 "블라인드 전형"에 주목하는 기사와 "코딩 테스트"에 주목하는 기사로 나뉘는 걸 발견
-// 튜브는 이들을 각각 묶어서 보여주면 카카오 공채 관련 기사를 찾아보는 사용자에게 유용할 것 같다고 판단
-// 유사한 기사를 묶는 기준을 정하기 위해서 논문과 자료를 조사하던 튜브는
-// 자카드 유사도라는 방법을 찾음
-
-// 자카드 유사도는 집합 간의 유사도를 검사하는 여러 방법 중의 하나로 알려짐
-// 두 집합 A B 사이의 자카드 유사도 J(A, B) 는 두 집합의 교집합 크기를 두 집합의 합집합 크기로 나눈 값으로 정의
-// 수식 = 교집합 크기 / 합집합 크기
-// 두 집합 모두 공집합일 경우에는 나눗셈이 정의되지 않으니 따로 1 로 정의한다 -> EARLY RETURN
-
-// 원소의 중복을 허용한다
-
-// 이를 이용하여 문자열 사이의 유사도를 계산하는데 이용 가능하다
-// 두 글자씩 끊어서 다중집합을 만들 수 있다
-// 이때 영문자로 된 글자 쌍만 유효
-// 기타 공백이나 숫자, 특수 문자가 들어있는 경우는 그 글자 쌍을 버린다
-// 대소문자 차이는 무시한다. -> toUpperCase()
-
-// 최종 출력 형식은 Math.floor(유사도 값 * 65536)
-
-function converter(nm, str) {
+const converter = (str) => {
+    const nm = new Map();
+    
     for (let i = 0; i < str.length - 1; i++) {
-        const convertedStr = str.slice(i, i + 2).toUpperCase();
-        const charCode1 = convertedStr[0].charCodeAt();
-        const charCode2 = convertedStr[1].charCodeAt();
+        const element = str.slice(i, i + 2).toUpperCase();
         
-        if (65 <= charCode1 && charCode1 <= 90
-           && 65 <= charCode2 && charCode2 <= 90) {
-            nm.set(convertedStr, nm.get(convertedStr) + 1 || 1);
+        if (/^[A-Z]{2}$/.test(element)) {
+            nm.set(element, nm.get(element) + 1 || 1);
         }
     }
+    return nm;
 }
 
 function solution(str1, str2) {
     var answer = 0;
     
+    const str1Map = converter(str1);
+    const str2Map = converter(str2);
+    
+    if (str1Map.size === 0 && str2Map.size === 0) return 65536;
+    
+    const elements = new Set([...str1Map.keys(), ...str2Map.keys()]);
+    
     let intersection = 0;
     let union = 0;
     
-    // 아스키 코드 값 A ~ Z = 65 ~ 90
-    const A = new Map();
-    const B = new Map();
-    
-    converter(A, str1);
-    converter(B, str2);
-    
-    console.log(A);
-    console.log(B);
-    
-    if (A.size === 0 && B.size === 0) return 65536;
-    
-    const keySet = new Set([...A.keys(), ...B.keys()]);
-    
-    for (const ks of keySet) {
-        const countA = A.get(ks) || 0;
-        const countB = B.get(ks) || 0;
+    for (const element of elements) {
+        const str1Count = str1Map.get(element) || 0;
+        const str2Count = str2Map.get(element) || 0;
         
-        intersection += Math.min(countA, countB);
-        union += Math.max(countA, countB);
+        intersection += Math.min(str1Count, str2Count);
+        union += Math.max(str1Count, str2Count);
     }
     
-    answer = intersection / union;
-    
-    return Math.floor(answer * 65536);
+    return Math.floor(intersection / union * 65536);
 }
