bbccd966f278ee14d3f0dd5a87f5eb49254749c0
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2026-06-06 11:33:08 +0900

    [level 2] Title: 방문 길이, Time: 0.34 ms, Memory: 32.8 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.js
@@ -1,14 +1,3 @@
-// 게임 캐릭터를 4가지 명령어를 통해 움직이려고 한다
-
-// U : 위로 한칸 -> y++
-// D : 아래로 한칸 -> y--
-// R : 오른쪽으로 한칸 -> x++
-// L : 왼쪽으로 한칸 -> x--
-
-// 캐릭터는 좌표 (0, 0) 위치에서 시작
-// 좌표평면의 경계는 
-// 왼쪽 위(-5, 5), 왼쪽 아래(-5, -5), 오른쪽 위(5, 5), 오른쪽 아래(5, -5)로 이루어져 있습니다.
-
 function solution(dirs) {
     let x = 0;
     let y = 0;
@@ -16,7 +5,8 @@ function solution(dirs) {
     const ns = new Set();
     
     for (let i = 0; i < dirs.length; i++) {
-        const order = dirs[i];
+        let order = dirs[i];
+        
         let nextX = x;
         let nextY = y;
         
@@ -34,18 +24,15 @@ function solution(dirs) {
                 nextX--;
                 break;
         }
-
+        
         if (-5 <= nextX && nextX <= 5 && -5 <= nextY && nextY <= 5) {
-            const key1 = `${x},${y}/${nextX},${nextY}`;
-            const key2 = `${nextX},${nextY}/${x},${y}`;
-            
-            if (!ns.has(key1)) {
-                ns.add(key1);
-                ns.add(key2);
-            }
+            ns.add(`${x},${y}/${nextX},${nextY}`);
+            ns.add(`${nextX},${nextY}/${x},${y}`);
             x = nextX;
             y = nextY;
         }
+        
     }
-    return ns.size / 2;    
+    
+    return ns.size / 2;
 }
