0ec998922ca990fef56ddc3c67d766e7bdd1a8c6
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2026-06-12 19:31:18 +0900

    [level 3] Title: 베스트앨범, Time: 7.98 ms, Memory: 45.9 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -1,41 +1,49 @@
+// 스트리밍 사이트에서 장르 별로 가장 많이 재생된 노래를 두 개씩 모아 베스트 앨범을 출시하려 합니다
+// 노래는 고유 번호로 구분하며, 노래를 수록하는 기준은 아래와 같다
+
+// 1. 속한 노래가 많이 재생된 장르를 먼저 수록
+// 2. 장르 내에서 많이 재생된 노래를 먼저 수록
+// 3. 장르 내에서 재생 횟수가 같은 노래 중에서는 고유 번호가 낮은 노래를 먼저 수록
+
+// 노래의 장르를 나타내는 문자열 배열과 노래별 재생 횟수를 나타내는 정수 배열이 주어질 때
+// 베스트 앨범에 들어갈 노래의 고유 번호를 순서대로 return
+
 function solution(genres, plays) {
-    var answer = [];
+    const answer = [];
     
-    // 1. Map 형태로 정리
     const nm = new Map();
     
     for (let i = 0; i < genres.length; i++) {
         const g = genres[i];
         const p = plays[i];
-
-        if (!nm.has(g)) {
-            nm.set(g, { total: 0, list: [] });
-        }
+        
+        if (!nm.has(g)) nm.set(g, { total: 0, list: [] });
         
         const data = nm.get(g);
+        
         data.total += p;
-        data.list.push({ id : i, play: p });
+        data.list.push({ i: i, play: p });
     }
-
+    
     const sortedMap = new Map([...nm].sort((a,b) => {
-        const [aKey, aValue] = a;
-        const [bKey, bValue] = b;
+        const [aI, aValue] = a;
+        const [bI, bValue] = b;
+        
         return bValue.total - aValue.total;
     }));
     
+    console.log(sortedMap);
+    
     for (const [key, value] of sortedMap) {
-        const {list} = value;
-        const sortedByPlay = list.sort((a,b) => {
+        const { list } = value;
+        const sortedByPlay = list.sort((a, b) => {
             if (a.play !== b.play) return b.play - a.play;
-            return a.id - b.id;
+            return a.i - b.i;
         });
         
-       answer.push(sortedByPlay[0].id);
+        answer.push(sortedByPlay[0].i);
         
-        if (sortedByPlay.length >= 2) {
-            answer.push(sortedByPlay[1].id);
-        }
+        if (sortedByPlay.length >= 2) answer.push(sortedByPlay[1].i);
     }
-    
     return answer;
 }
