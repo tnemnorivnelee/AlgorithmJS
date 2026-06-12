57be38c16bbae145a66ed7e579a82edca4d09b76
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2026-06-12 15:41:37 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 41.03 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=javascript) 
 
 ### 성능 요약
 
-메모리: 66.7 MB, 시간: 61.32 ms
+메모리: 81.8 MB, 시간: 41.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 23일 15:56:04
+2026년 06월 12일 15:41:33
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,16 +1,24 @@
-function solution(participant, completion) {
-    const mapParticipant = new Map();
-    
-    for (let i = 0; i < participant.length; i++) {
-        const a = participant[i];
-        const b = completion[i];
-        
-        mapParticipant.set(a, (mapParticipant.get(a) || 0) + 1);
-        mapParticipant.set(b, (mapParticipant.get(b) || 0) - 1);
-    }
+// 수많은 마라톤 선수들이 마라톤에 참여
+// 단 한 명의 선수를 제외하고는 모든 선수가 마라톤 완주
 
+// 마라톤에 참여한 선수들의 이름이 담긴 배열과 완주한 선수들의 이름이 담긴 배열이 주어짐
+// 완주하지 못한 선수의 이름을 리턴
+
+// 참가자 중에는 동명이인이 있을 수 있습니다. -> new Map 사용
+
+function solution(participant, completion) {
+    const nm = new Map();
     
-    for (const [key, value] of mapParticipant) {
-        if (value) return key;
-    }
+    participant.forEach((v) => nm.set(v, nm.get(v) + 1 || 1));
+    
+    completion.forEach((v) => {
+        if (nm.has(v)) {
+            if (nm.get(v) === 1) {
+                nm.delete(v); 
+            } else {
+                nm.set(v, nm.get(v) - 1);
+            }
+        }
+    });
+    return [...nm][0][0];
 }
