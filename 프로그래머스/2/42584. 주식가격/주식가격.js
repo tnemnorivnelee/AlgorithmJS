cb65ed8b729e6d6a2c5bfe0a4c28defabcf635b7
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2026-06-13 16:43:36 +0900

    [level 2] Title: 주식가격, Time: 7.40 ms, Memory: 58.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,17 +1,34 @@
 function solution(prices) {
-    const answer = Array.from({length: prices.length}).fill(0);
+    const n = prices.length; // prices 배열의 길이를 n이라는 변수에 담아둡니다.
+    
+    // 일단 모든 주식이 끝까지 떨어지지 않았다고 가정하고 정답 배열을 미리 세팅합니다.
+    const answer = new Array(n).fill(0);
+    
+    // 아직 가격이 떨어지지 않은 주식들의 '시간(인덱스)'을 담아둘 빈 스택입니다.
     const stack = [];
     
-    for (let cur = 0; cur < prices.length; cur++) {
+    // 시간(초)이 0초부터 끝까지 흐릅니다. (i는 현재 시간)
+    for (let i = 0; i < n; i++) {
+        // 스택에 보관된 과거의 주식이 있고 && 과거의 주식 가격이 현재 주식 가격보다 비싸다면?
+        // 즉, 스택에 남아있던 과거 주식의 가격이 지금 막 떨어졌음을 의미합니다!
+        while (stack.length > 0 && prices[stack[stack.length - 1]] > prices[i]) {
+            // 가격이 떨어진 과거 주식의 시간(인덱스)을 스택에서 꺼냅니다.
+            const pastTime = stack.pop();
+            
+            // 과거 주식이 가격을 유지한 시간 = 현재 시간(i) - 과거 주식을 샀던 시간(pastTime)
+            answer[pastTime] = i - pastTime;
+        }
         
-       while (stack.length > 0 && prices[stack[stack.length - 1]] > prices[cur]) {
-           const past = stack.pop();
-           answer[past] = cur - past;
-       }
-        stack.push(cur);
+        // 현재 시간(인덱스)도 아직 가격이 떨어지지 않았으므로 스택에 보관합니다.
+        stack.push(i);
     }
-    for (let i = 0; i < stack.length; i++) {
-        answer[stack[i]] = prices.length - 1 - stack[i];
+    
+    // 반복문이 다 끝났는데도 스택에 남아있는 시간(인덱스)들이 있습니다.
+    // 얘네들은 주식 시장이 끝날 때까지 한 번도 가격이 떨어지지 않은 최고 효자 종목들입니다!
+    while (stack.length > 0) {
+        const pastTime = stack.pop(); // 꺼냅니다.
+        // 전체 시간(n - 1)에서 자신이 들어간 시간(pastTime)을 빼면 유지된 시간이 나옵니다.
+        answer[pastTime] = n - 1 - pastTime;
     }
     
     return answer;
