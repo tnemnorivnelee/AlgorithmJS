65da94269b06bdf0f76ba7c6f77814dbd74c466a
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2026-06-13 17:27:15 +0900

    [level 1] Title: K번째수, Time: 0.12 ms, Memory: 44 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,10 +1,3 @@
 function solution(array, commands) {
-    var answer = [];
-    
-    for (let i = 0; i < commands.length; i++) {
-        const [head, tail, idx] = commands[i];
-        answer.push(array.slice(head - 1, tail).sort((a,b) => a - b)[Number(idx) - 1]);
-    }
-    
-    return answer;
+    return commands.map(([start, end, index]) => array.slice(start - 1, end).sort((a,b) => a - b)[index - 1]);
 }
