6c64b2e67177b72c0deddca46aa1831e2b58583c
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2026-06-16 15:28:26 +0900

    [level 1] Title: 모의고사, Time: 1.31 ms, Memory: 49.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=javascript) 
 
 ### 성능 요약
 
-메모리: 36.8 MB, 시간: 5.62 ms
+메모리: 49.5 MB, 시간: 1.31 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 26일 16:54:33
+2026년 06월 16일 15:28:22
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -5,27 +5,23 @@ function solution(answers) {
         [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     ];
     
-    const scores = new Array(sufojas.length).fill(0);
-
-
+    const scores = Array.from({ length: sufojas.length }).fill(0);
+    
     for (let i = 0; i < sufojas.length; i++) {
         for (let j = 0; j < answers.length; j++) {
-            if (answers[j] === sufojas[i][j % sufojas[i].length]) {
+            if (sufojas[i][j % sufojas[i].length] === answers[j]) {
                 scores[i]++;
             }
         }
     }
     
-    const maxScore = Math.max(...scores);
-    
     const answer = [];
     
     for (let i = 0; i < scores.length; i++) {
-        if (scores[i] === maxScore) {
+        if (Math.max(...scores) === scores[i]) {
             answer.push(i + 1);
         }
     }
     
     return answer;
-
 }
