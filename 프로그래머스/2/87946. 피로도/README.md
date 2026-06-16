889fd4a4eeccd870975aaa0fa96ae5d15978025b
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2026-06-16 17:07:31 +0900

    [level 2] Title: 피로도, Time: 2.21 ms, Memory: 49.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=javascript) 
 
 ### 성능 요약
 
-메모리: 36.6 MB, 시간: 28.61 ms
+메모리: 49.2 MB, 시간: 2.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 22일 14:40:16
+2026년 06월 16일 17:07:28
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,21 +1,24 @@
-// 최소 필요 피로도 : 던전을 탐험하기 위해 가지고 있어야 하는 최소한의 피로도
-// 소모 피로도 : 던전을 탐험한 후 소모되는 피로도
+// XX 게임에는 피로도 시스템이 있으며, 일정 피로도를 사용해서 던전을 탐험할 수 있다
+// 이때, 각 던전마자 탐험을 시작하기 위해 필요한 "최소 필요 피로도"와
+// 던전 탐험을 마쳤을 때 소모되는 "소모 피로도"가 있다
 
-// ex) 최소 필요 피로도 80, 소모 피로도 20 인 던전을 탐험할 경우
-// 유저의 현재 남은 피로도는 80 이상이여야 가능. 던전 탐험 후에는 피로도 20 소모.
+// "최소 필요 피로도"는 해당 던전을 탐험하기 위해 가지고 있어야 하는 최소한의 피로도.
+// "소모 피로도"는 던전을 텀함한 후 소모되는 피로도.
 
-// k = 유저의 현재 피로도
-// dungeons = [[최소 필요 피로도, 소모 피로도], [최소 필요 피로도, 소모 피로도]]
+// 이 게임에는 하루에 한 번씩 탐험할 수 있는 던전이 여러개 있는데, 
+// 한 유저가 오늘 이 던전들을 최대한 많이 탐험하려 한다.
+// 유저의 현재 피로도와 각 던전 별 "최소 필요 피로도", "소모 피로도"가 담긴 2차원 배열이 주어질 때
+// 유저가 탐험할 수 있는 최대 던전 수를 반환하는 함수를 완성해라.
 
 function solution(k, dungeons) {
-    let maxCount = 0;
-    const visited = new Array(dungeons.length).fill(false);
+    let answer = 0;
+    const visited = Array.from({ length: dungeons.length }).fill(false);
     
     function dfs(currentK, currentCount) {
-        maxCount = Math.max(maxCount, currentCount);
+        answer = Math.max(answer, currentCount);
         
         for (let i = 0; i < dungeons.length; i++) {
-            if (!visited[i] && currentK >= dungeons[i][0]) {
+            if (!visited[i] && dungeons[i][0] <= currentK) {
                 visited[i] = true;
                 dfs(currentK - dungeons[i][1], currentCount + 1);
                 visited[i] = false;
@@ -25,5 +28,5 @@ function solution(k, dungeons) {
     
     dfs(k, 0);
     
-    return maxCount;
+    return answer;
 }
