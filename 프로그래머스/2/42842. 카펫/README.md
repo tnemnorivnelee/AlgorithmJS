f150ba613668d99c76bb2198839da1afc1ebe657
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2026-06-21 15:22:36 +0900

    [level 2] Title: 카펫, Time: 0.19 ms, Memory: 44.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.22 ms
+메모리: 44.1 MB, 시간: 0.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 03일 18:55:45
+2026년 06월 21일 15:22:30
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,18 +1,27 @@
+// 레오는 카펫을 사러 갔다가 그림과 같이 중앙에는 노란색으로 칠해져 있고
+// 테두리는 갈색으로 칠해져 있는 격자 모양 카펫을 봤다.
+
+// 레오는 집으로 돌아와서 아까 본 카펫의 노란색과 갈색으로 색칠된 격자의 개수는 기억했지만
+// 전체 카펫의 크기는 기억하지 못했다.
+
+// 레오가 본 카펫에서 갈색과 노란색 격자의 수가 매개변수로 주어질 때 
+// 카펫의 가로, 세로 크기를 순서대로 배열에 담아 리턴하도록 함수를 완성하라
+
+// 가로 >= 세로
+// 가로 * 세로 = brown + yellow
+// 
+
 function solution(brown, yellow) {
-    // 가로 >= 세로
-    // 가로 * 세로 = brown + yellow
-    // 가로 세로는 yellow 의 약수
-    
-    const divisors = [];
+    var answer = [];
     
     for (let i = 1; i <= Math.sqrt(yellow); i++) {
         const w = yellow / i + 2;
         const h = i + 2;
         
-        if (w * h === brown + yellow && w >= h) {
-            divisors.push(w);
-            divisors.push(h);
+        if (w >= h && w * h === brown + yellow) {
+            answer.push(w);
+            answer.push(h);
         }
     }
-    return divisors;    
+    return answer;
 }
